438d6c0e0d5c12223b7477b7046d5d64bc9d6160
zhangchao <zhangchao1@kaolafm.com>
2019-06-26 23:21:23 +0800

    添加滑动效果

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
+const APPBAR_OFFSET = 100;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -13,27 +15,74 @@ class _HomePageState extends State<HomePage> {
     'https://img1.doubanio.com/view/photo/l/public/p2512009298.webp',
   ];
 
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      children: <Widget>[
-        Container(
-          height: 250,
-          child: Swiper(
-            itemCount: _imageUrls.length,
-            autoplay: true,
-            itemBuilder: (BuildContext context, int index) {
-              return Image.network(
-                _imageUrls[index],
-                fit: BoxFit.fill,
-              );
-            },
-            pagination: SwiperPagination(),
-          ),
-        )
-      ],
-    )));
+        body: Stack(
+          children: <Widget>[
+            MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: NotificationListener(
+                onNotification: (sc) {
+                  if (sc is ScrollUpdateNotification && sc.depth == 0) {
+                    _onScroll(sc.metrics.pixels);
+                  }
+                },
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      height: 250,
+                      child: Swiper(
+                        itemCount: _imageUrls.length,
+                        autoplay: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Image.network(
+                            _imageUrls[index],
+                            fit: BoxFit.fill,
+                          );
+                        },
+                        pagination: SwiperPagination(),
+                      ),
+                    ),
+                    Container(
+                      height: 1000,
+                      child: ListTile(
+                        title: new Text("哈哈哈"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Opacity(
+              opacity: appBarAlpha,
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text('首页'),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
